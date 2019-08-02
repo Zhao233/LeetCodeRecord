6c73b8f92943912cb15f48fad223f8010c21ed8f
zhaoxu <953071759@qq.com>
2019-08-02 21:54:57 +0800

    暂时完成，未提交测试

--- 15.三数之和/code_1.scala
+++ 15.三数之和/code_1.scala
@@ -1,26 +1,10 @@
-
-import Array._
-import scala.collection.mutable.ArrayBuffer
-
 object TestKafka {
-  val aliServer = "127.0.0.1"
-  val port = "9092"
-
   def main(args: Array[String]): Unit = {
-
-
-
     print(threeSum(Array(-1, 0, 1, 2)))
-
-    var list = ArrayBuffer[Int]();
-
-    list+=(1)
-
-    println( list.toString )
   }
 
-  //def threeSum(nums: Array[Int]) : List[List[Int]] = {
-  def threeSum(nums: Array[Int]) : Unit = {
+  def threeSum(nums: Array[Int]) : List[List[Int]] = {
+  //def threeSum(nums: Array[Int]): Unit = {
     var list = ArrayBuffer[ArrayBuffer[Int]]()
 
     //数组的长度
@@ -46,10 +30,12 @@ object TestKafka {
 
     var list_not_search = ArrayBuffer[Int]();
 
-    for (j <- 0 until length) {
+    for (j <- 0 until length
+          if !list_not_search.contains( matrix(0)(j) )
+        ) {
       for (i <- 0 until length //x-axis
            if j != i
-           if list_not_search.contains(i) ) { //跳过与自己进行加法
+           if !list_not_search.contains( matrix(i)(j) ) ) { //跳过与自己进行加法
 
         var a = nums(j)
         var b  = matrix(i)(j);
@@ -58,11 +44,11 @@ object TestKafka {
 
         breakable {
           for (x <- 0 until length
-               if x != i
-                 ) {
+               if x != i //跳过自己
+          ) {
             if (c == matrix(x)(j)) {
-              list += ArrayBuffer(a, b, c)
-              list_not_search += (x)
+              //list += ArrayBuffer(a, b, c)
+              list_not_search += (a,b,c)
               break
 
             }
@@ -71,7 +57,15 @@ object TestKafka {
       }
     }
 
-    print(list.toList)
-  }
-}
+    var array_result = ArrayBuffer[List[Int]]()
 
+    for( i <- 0 until list_not_search.length / 3 ){
+      var temp_list = List(list_not_search(i), list_not_search(i+1), list_not_search(i+2))
+
+      array_result += temp_list
+    }
+
+    return array_result.toList
+  }
+  
+}
